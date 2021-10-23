ff15398cd8186b306ece9b1eb26fc978bfe88cf6
99xiewilliam <35141949+99xiewilliam@users.noreply.github.com>
2021-10-23 12:30:29 +0800

    submit modify post

--- app/src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155162650/HttpPostTask.java
+++ app/src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155162650/HttpPostTask.java
@@ -7,10 +7,14 @@ import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+
 import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
+
 
 import java.net.URL;
 
@@ -29,15 +33,22 @@ public abstract class HttpPostTask extends AsyncTask<String, String, Boolean> im
             DefaultHttpClient client = new DefaultHttpClient();
 
             HttpPost post = new HttpPost(url);
-            post.setHeader("Content-type", "application/json");
-            post.setHeader("Accept", "application/json");
-
-            JSONObject param_data = new JSONObject();
-            param_data.put("chatroom_id", params[1]);
-            param_data.put("user_id", params[2]);
-            param_data.put("name", params[3]);
-            param_data.put("message", params[4]);
-            post.setEntity(new StringEntity(param_data.toString()));
+            MultipartEntityBuilder builder = MultipartEntityBuilder.create();
+//            post.setHeader("Content-type", "multipart/form-data");
+//            post.setHeader("Accept", "application/json");
+//
+//            JSONObject param_data = new JSONObject();
+//            param_data.put("chatroom_id", params[1]);
+//            param_data.put("user_id", params[2]);
+//            param_data.put("name", params[3]);
+//            param_data.put("message", params[4]);
+//            post.setEntity(new StringEntity(param_data.toString()));
+            builder.addTextBody("chatroom_id", params[1], ContentType.TEXT_PLAIN);
+            builder.addTextBody("user_id", params[2], ContentType.TEXT_PLAIN);
+            builder.addTextBody("name", params[3], ContentType.TEXT_PLAIN);
+            builder.addTextBody("message", params[4], ContentType.TEXT_PLAIN);
+            HttpEntity multipart = builder.build();
+            post.setEntity(multipart);
 
             HttpResponse response = client.execute(post);
             int status = response.getStatusLine().getStatusCode();
